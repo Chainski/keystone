d024e57cca6787c8b0f0ebae7399eb31c735a246
CHA1NSK1 <96607632+Chainski@users.noreply.github.com>
2025-09-02 01:20:49 -0500

    minor change

--- build_windows_binaries.bat
+++ build_windows_binaries.bat
@@ -1,41 +1,34 @@
 @echo off
 setlocal EnableDelayedExpansion
+color e
 echo Setting up Visual Studio environment...
 color 2
 call "C:\Program Files\Microsoft Visual Studio\2022\Enterprise\VC\Auxiliary\Build\vcvars64.bat"
 color 6
 echo Creating build directory...
-color 6
 rd /S /Q build >nul 2>&1
 mkdir build
-dir build
 cd build
-color 6
+color 9
 echo Running nmake-lib.bat...
 color e
 cmd /c ..\nmake-lib.bat
 color 6
 echo Returning to parent directory...
-color e
 cd ..
-color 6
+color b
 echo Creating dll-build directory...
-color 6
 rd /S /Q dll-build >nul 2>&1
 mkdir dll-build
-dir dll-build
 cd dll-build
 color 6
 echo Running nmake-dll.bat...
 color 6
 cmd /c ..\nmake-dll.bat
 timeout /t 4 /nobreak >nul
-color %BLUE%
+color 6
 echo Listing built directory contents...
-color %BLUE%
 dir "..\build\kstool\"
 dir "..\build\llvm\lib\"
 dir "..\dll-build\llvm\bin\"
 endlocal
-
-
